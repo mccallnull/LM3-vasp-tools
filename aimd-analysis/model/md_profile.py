c4e26f2f453db67539c259d88351965c3ab707fd
mccallnull <mccallnull@gmail.com>
2026-07-10 23:37:58 +0900

    AIMD analysis: add MD profile class

---
+++ aimd-analysis/model/md_profile.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from dataclasses import field
+from typing import Optional
+import numpy as np
+
+@dataclass
+class MDProfile:
+
+    # NVT에서 기본적으로 추출할 것들.
+    step: np.ndarray = field(default=None)
+    Epot: np.ndarray = field(default=None)
+    Ekin: np.ndarray = field(default=None)
+    Etot: np.ndarray = field(default=None)
+    T_md: np.ndarray = field(default=None)
+
+    # NPT라면, 이것들도 추가로 추출할 것.
+    P_md: Optional[np.ndarray] = field(default=None)
+    V_md: Optional[np.ndarray] = field(default=None)
+    lat_a: Optional[np.ndarray] = field(default=None)
+    lat_b: Optional[np.ndarray] = field(default=None)
+    lat_c: Optional[np.ndarray] = field(default=None)
+    lat_alp: Optional[np.ndarray] = field(default=None)
+    lat_bet: Optional[np.ndarray] = field(default=None)
+    lat_gam: Optional[np.ndarray] = field(default=None)
+
+    # 실제 timestep (in fs) --> 실제 시간을 쓰려면, step * dt 를 하도록. (INCAR reading 필요.)
+    dt: Optional[float] = field(default=None)
+
+    # 다른 유도 properties(추출하는 것들로부터 계산되는 것들)는 여기.
+    @property
+    def nsteps(self):
+        return len(self.step)
+    
+    @property
+    def time(self):
+        if self.dt is None:
+            return None
+        return self.step * self.dt
+
+    @property
+    def duration(self):
+        if self.dt is None:
+            return None
+        return self.nsteps * self.dt
+    
+    @property
+    def has_pressure(self): # NPT인지를 판가름할 때 편할 듯.
+        return self.P_md is not None
